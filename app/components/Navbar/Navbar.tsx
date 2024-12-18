dc4db02e2357491fbda3a72c58131310df08b126
karlajaramillo <karlav.jaramillo@gmail.com>
2024-12-18 14:15:48 +0100

    chore: add navbar

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../../../public/mango-Logo.png';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.border}>
+      <div className={`${styles.wrapper} ${styles.flex}`}>
+        <div>
+          <Link href="/" className={styles.imgWrapper}>
+            <Image
+              src={Logo}
+              alt="Mango logo"
+              width={70}
+              quality={100}
+              placeholder="blur"
+            />
+          </Link>
+        </div>
+
+        <ul className={styles.list}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/exercise1">Exercise 1</Link>
+          </li>
+          <li>
+            <Link href="/exercise2">Exercise </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
