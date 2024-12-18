4b33b3b4ccad1924ff8f687511e50f7e65b34e66
Karla Jaramillo <karlav.jaramillo@gmail.com>
2024-12-18 14:36:04 +0100

    Feature layout (#2)
    * chore: add navbar
    
    * chore: add style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar/Navbar';
 import './styles/globals.css';
 import { Poppins } from 'next/font/google';
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
   );
 }
